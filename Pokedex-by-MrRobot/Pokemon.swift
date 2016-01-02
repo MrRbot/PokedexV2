1ffb8da3c9047421c3eeb96f206acb7f3bb9f38c
Javier <Javier@MBP-de-Javier.lan>
2016-01-02 01:43:09 -0600

    Added a Pokemon class, parsed a CSV file and configured a collection view to display the Pokemons

---
+++ Pokedex-by-MrRobot/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex-by-MrRobot
+//
+//  Created by Javier on 31/12/15.
+//  Copyright © 2015 MrRobot. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex-by-MrRobot/PokemonCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  Pokedex-by-MrRobot
+//
+//  Created by Javier on 01/01/16.
+//  Copyright © 2016 MrRobot. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+}
